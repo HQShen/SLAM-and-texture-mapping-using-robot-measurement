52a6e61c5638efeb9cbfa02385f54c6d6a268b21
Huaqing <h1shen@ucsd.edu>
2019-10-31 01:39:07 -0700

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -67,8 +67,8 @@ class Robot(object):
             xmap, ymap = point_in_map(xw, yw, xmin, ymin, res)
 
             p.w, index = mapCorrelation(im, xmap, ymap)
-            p.x = p.x + res * (index[1] - 4) # change the order of index[1] and index[0] if not works well
-            p.y = p.y + res * (index[0] - 4)
+            p.x = p.x + res * (index[0] - 4) # change the order of index[1] and index[0] if not works well
+            p.y = p.y + res * (index[1] - 4)
         self._find_best_particle()
         self._resample()
 
